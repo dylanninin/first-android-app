3b6f0e998df54fe038e1f411b694dd515f0b1ff5
Dylan <dylanninin@gmail.com>
2021-10-08 21:00:05 +0800

    chore: add toast

--- app/src/main/java/com/example/kotlindemo/FirstFragment.kt
+++ app/src/main/java/com/example/kotlindemo/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.kotlindemo.databinding.FragmentFirstBinding
 
@@ -32,8 +34,13 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toastButton.setOnClickListener {
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            val myToast = Toast.makeText(context, "Hello Toast", Toast.LENGTH_SHORT)
+            myToast.show()
         }
     }
 
