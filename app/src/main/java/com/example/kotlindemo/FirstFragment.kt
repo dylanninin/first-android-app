e23742f87528644c737b2d44bce58197796626f3
Dylan <dylanninin@gmail.com>
2021-10-08 21:03:40 +0800

    chore: add count

--- app/src/main/java/com/example/kotlindemo/FirstFragment.kt
+++ app/src/main/java/com/example/kotlindemo/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.kotlindemo.databinding.FragmentFirstBinding
@@ -42,10 +43,21 @@ class FirstFragment : Fragment() {
             val myToast = Toast.makeText(context, "Hello Toast", Toast.LENGTH_SHORT)
             myToast.show()
         }
+
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun countMe(view: View) {
+        val textView = view.findViewById<TextView>(R.id.textview_first)
+        var count = textView.text.toString().toInt()
+        count++
+        textView.text = count.toString()
     }
 }
